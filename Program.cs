b5e03ff74cb988b15070145987f8a4c14bcefaf1
AdrianMorenoNystrom <adrianmorenonystrom@gmail.com>
2023-09-07 13:39:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ChessBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Mata in en siffra");
+            string input = Console.ReadLine();
+            int number = Int32.Parse(input);
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write("■");
+                Console.Write("□");
+            }
+
+        }
+    }
+}
